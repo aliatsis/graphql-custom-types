1289892642a0c41a5e3c62d1bd2f7a26249a4ed4
Anthony Liatsis <anthony.liatsis@gmail.com>
2016-05-08 22:52:20 -0700

    typings and more

---
+++ graphql-custom-types.d.ts
@@ -0,0 +1,40 @@
+import { GraphQLScalarType, Value } from "graphql";
+
+export class Factory {
+  getRegexScalar(options: RegexScaleOptions): GraphQLCustomScalarType;
+  getCustomScalar(name: string, description: string, parser: (ast: Value) => any);
+}
+
+export class GraphQLCustomScalarType extends GraphQLScalarType {
+  constructor(name: string, description: string, parser: (ast: Value) => any);
+}
+
+export class GraphQLLimitedString extends GraphQLCustomScalarType {
+  constructor(min?: number, max?: number, alphabet?: string);
+}
+
+export class GraphQLPassword extends GraphQLCustomScalarType {
+  constructor(min?: number, max?: number, alphabet?: string, complexity?: PasswordComplexityOptions);
+}
+
+export class GraphQLDefinedString extends GraphQLCustomScalarType {
+  constructor(definedValues: string[], customName?: string, customDescription?: string)
+}
+
+export var GraphQLEmail: GraphQLCustomScalarType;
+export var GraphQLURL: GraphQLCustomScalarType;
+export var GraphQLDateTime: GraphQLCustomScalarType;
+export var GraphQLUUID: GraphQLCustomScalarType;
+
+interface RegexScaleOptions {
+  name: string;
+  regex: RegExp;
+  description?: string;
+  error?: string | Error;
+}
+
+interface PasswordComplexityOptions {
+  alphaNumeric?: boolean;
+  mixedCase?: boolean;
+  specialChars?: boolean;
+}
